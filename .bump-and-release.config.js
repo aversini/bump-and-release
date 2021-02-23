d9dc8990a054e82a53cc42c90862e66ad255a258
Arno Versini <arno.versini@wellsfargo.com>
2021-02-23 08:49:57 -0800

    chore: fixing bump options positions

--- .bump-and-release.config.js
+++ .bump-and-release.config.js
@@ -2,24 +2,35 @@ module.exports = {
   bump: {
     nextPossible: [
       {
+        pos: 0,
+        default: true,
+        type: "prerelease",
+        identifier: "beta",
+        prompt: (type, version) =>
+          `[${type}] ... development phase only (${version})`,
+      },
+      {
+        pos: 1,
         type: "patch",
         prompt: (type, version) =>
-          `[${type}] ... only defect fixes (${version})`,
+          `[${type}] ........ only defect fixes (${version})`,
       },
       {
+        pos: 2,
         type: "minor",
-        default: true,
         prompt: (type, version) =>
-          `[${type}] ... at least one new feature (${version})`,
+          `[${type}] ........ at least one new feature (${version})`,
       },
       {
+        pos: 3,
         type: "major",
         prompt: (type, version) =>
-          `[${type}] ... breaking change(s) (${version})`,
+          `[${type}] ........ breaking change(s) (${version})`,
       },
       {
+        pos: 4,
         type: "custom",
-        prompt: (type) => `[${type}] .. enter a custom version`,
+        prompt: (type) => `[${type}] ....... enter a custom version`,
       },
     ],
   },
